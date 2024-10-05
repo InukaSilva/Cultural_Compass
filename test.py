a4ab62338a58cb8b7331714dede026fadc013aee
saks <angelachen1260@gmail.com>
2024-10-05 02:51:07 -0400

    two select boxes omg

--- test.py
+++ test.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.write("Culture Compass")
 
 choices = ['Toronto', 'Vancouver', 'Your mom']
-menu = st.selectbox("Choose a City", choices)
+choice2 = ['Canadian', 'Chinese', 'Japanese']
+
+cities = st.selectbox("Choose a City", choices)
+cultures = st.selectbox("What Culture Would You Like To see?", choice2)
