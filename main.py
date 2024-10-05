ffa1b2da5b83ac88a8e071e98b35aae3064f4c15
InukaSilva <inukasilva@outlook.com>
2024-10-05 03:35:13 -0400

    edited theme

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.write("Hello World")
+
+def search():
+    if st.session_state.postal_code == "" or st.session_state.culture == "":
+        st.write("Please Enter Values")
+    else:
+        st.session_state.postal_code 
+        st.session_state.culture
+
+
+st.set_page_config(
+        page_title="Culture Compass",
+)
+
+st.write("Culture Compass")
+st.text_input("Postal Code", key="postal_code")
+st.text_input("Culture", key="culture")
+if st.button("Search"):
+    search()
+
+
+
+
