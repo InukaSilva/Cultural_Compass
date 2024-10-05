cc6191e1b5cf8974088851fcc07165f8f361ee54
InukaSilva <inukasilva@outlook.com>
2024-10-05 11:59:45 -0400

    added register event

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
 import streamlit as st
-from openai import OpenAI
-import os
 
-
-def set_up_model(culture: str) -> str:
-    return "THIS ISN'T SUPPOSE TO BE HERE"
-
-
-
-
-def search():
-    if st.session_state.postal_code == "" or st.session_state.culture == "":
-        st.write("Please Enter Values")
+def get_search_inputs():
+    """
+    Gets the inputs from the text box for the search
+    """
+    if st.session_state.city == "" or st.session_state.culturesearch == "":
+        st.write("Please Enter Both Values")
     else:
-        postal_code = st.session_state.postal_code 
-        culture = st.session_state.culture
-        st.map(latitude="100", longitude="100")
-        set_up_model(culture)
+        st.session_state.city
+        st.session_state.culturesearch
+
+
+
+
+def register_event():
+    """
+    Creates a form for the organizer of an event to add a event to the database.
+    """
+    st.text_input("Culture", key="culture")
+    st.text_input("Date (YYYY-MM-DD)", key="date")
+    st.text_input("Location (City)", key="location")
+    st.text_input("Registration Type (Open or RSVP)", key="registration")
+    st.text_input("Website (optional)", key="website")
+    st.text_input("Brief Blurb about the event", key="blurb")
+
+    # take data and store in json file using correct format
+
+        
 
 
 st.set_page_config(
         page_title="Culture Compass",
 )
 
-st.write("Culture Compass")
-st.text_input("Postal Code", key="postal_code")
-st.text_input("Culture", key="culture")
-if st.button("Search"):
-    search()
 
+st.write("Culture Compass")
+st.text_input("City", key="city")
+st.text_input("Culture", key="culturesearch")
+if st.button("Search"):
+    get_search_inputs()
+
+if st.button("+"):
+    register_event()
 
 
 
