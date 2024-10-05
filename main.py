bdf90931ee6251470d0c6c30b2691d1f41227cf8
InukaSilva <inukasilva@outlook.com>
2024-10-05 14:23:10 -0400

    got the map of the events, reads json data and prints it to the screen

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import json
+import os
+from dotenv import load_dotenv
+from opencage.geocoder import OpenCageGeocode
+
+# Loads the API key so that data can be pulled for the location
+load_dotenv()
+
+api_key = os.getenv('GEOCODER_API')
+geocoder = OpenCageGeocode(api_key)
+
+# Sets the configuration of the page
+st.set_page_config(
+        page_title="Culture Compass",
+)
+
 
 def get_search_inputs():
     """
@@ -7,11 +23,20 @@ def get_search_inputs():
     if st.session_state.city == "" or st.session_state.culturesearch == "":
         st.write("Please Enter Both Values")
     else:
-        st.session_state.city
-        st.session_state.culturesearch
+        display_data(str(st.session_state.culturesearch))
 
+def display_data(culture):
+    
+    with open('data.json', 'r') as file:
+        data = json.load(file)
+    for i in data["cultural_events"][culture]:
 
+        geocoder = OpenCageGeocode(api_key)
 
+        location = geocoder.geocode(i['location'])
+    
+        st.write(i)
+        st.map(latitude=location[0]['geometry']["lat"], longitude=location[0]['geometry']['lng'])
 
 def register_event():
     """
@@ -19,20 +44,12 @@ def register_event():
     """
     st.text_input("Culture", key="culture")
     st.text_input("Date (YYYY-MM-DD)", key="date")
-    st.text_input("Location (City)", key="location")
+    st.text_input("Location (City, Province Initals)", key="location")
     st.text_input("Registration Type (Open or RSVP)", key="registration")
     st.text_input("Website (optional)", key="website")
     st.text_input("Brief Blurb about the event", key="blurb")
 
     # take data and store in json file using correct format
-
-        
-
-
-st.set_page_config(
-        page_title="Culture Compass",
-)
-
 
 st.write("Culture Compass")
 st.text_input("City", key="city")
@@ -40,7 +57,7 @@ st.text_input("Culture", key="culturesearch")
 if st.button("Search"):
     get_search_inputs()
 
-if st.button("+"):
+if st.button("Add Cultural Event"):
     register_event()
 
 
